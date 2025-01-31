4a24fe7bf907989ec521a628101528a02414231e
devD <deoredevendra1988@gmail.com>
2025-01-31 21:06:13 +0530

    Initial commit - Selenium script added

---
+++ log_in.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+
+
+driver= webdriver.Chrome()
+driver.maximize_window()
+driver.get("https://www.saucedemo.com/")
+driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div[1]/div/div/form/div[1]/input").send_keys("standard_user")
+driver.find_element(By.XPATH, "//*[@id='password']").send_keys("secret_sauce")
+driver.find_element(By.ID, "login-button").click()
+input("Press Enter to close the browser...")
